3e586a59404bcdb0215865aaa427bbf723f1a7b8
Sergio Demarco <sergio.demarco@thebridge.social>
2021-07-15 17:01:39 -0300

    master

---
+++ pancake-frontend-develop/src/components/Menu/config.ts
@@ -0,0 +1,58 @@
+import { MenuEntry } from '@pancakeswap/uikit'
+import { ContextApi } from 'contexts/Localization/types'
+
+const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
+  {
+    label: t('Home'),
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: t('Trade'),
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: t('Exchange'),
+        href: 'https://exchange.pancakeswap.finance/#/swap',
+      },
+      {
+        label: t('Liquidity'),
+        href: 'https://exchange.pancakeswap.finance/#/pool',
+      }
+    ],
+  },
+  {
+    label: t('Farms'),
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: t('Pools'),
+    icon: 'PoolIcon',
+    href: '/pools',
+  },
+  {
+    label: t('More'),
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: t('Contact'),
+        href: 'https://docs.pancakeswap.finance/contact-us',
+      },
+      {
+        label: t('Github'),
+        href: 'https://github.com/pancakeswap',
+      },
+      {
+        label: t('Docs'),
+        href: 'https://docs.pancakeswap.finance',
+      },
+      {
+        label: t('Blog'),
+        href: 'https://pancakeswap.medium.com',
+      },
+    ],
+  },
+]
+
+export default config
